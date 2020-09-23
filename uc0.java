79a18cc1e3b7e4c914a64dbd634563f78def5710
sreeja99 <srijagodishala@gmail.com>
2020-09-23 16:39:00 +0530

    another version

--- uc0.java
+++ uc0.java
@@ -11,27 +11,44 @@ public class assignOne {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-//		int dailyempwage = 0;
-//		int monthlywage =0;
-//		int noHours =0;
-//		int noDays = 0;
+		int dailyempwage = 0;
+		int monthlywage =0;
+		int noHours =0;
+		int noDays = 0;
 		System.out.println("Welcome to Employee Wage Computation program");
-
+		while(noDays<=20 && noHours<=100) {
 		
 		double empCheck = Math.floor(Math.random() * 10) % 3;
-		if( empCheck == 1){
-			System.out.println("Employee is present");
-			System.out.println("Wage- "+HOURS_WORKING *WAGE_PER_HOUR);
-		}
-		else {
-			System.out.println("Employee is absent");
-			System.out.println("Wage -"+ NO_OF_WORK_DAYS *PART_DAY_HOUR*WAGE_PER_HOUR);
-		}
+//		if( empCheck == 1){
+//			System.out.println("Employee is present");
+//		}
+//		else {
+//			System.out.println("Employee is absent");
+//		}
 		
 //		System.out.println("full time employee wage-"+ dailyempwage);
 //		System.out.println("part time employee wage-"+ partempwage);
-
+	    switch((int)empCheck) {
+	        case IS_FULL_TIME :
+	    	   System.out.println("Employer is present");
+	    	   noHours = noHours+1;
+	    	   noDays = noDays+FULL_DAY_HOUR;
+	    	   break;
+	        case IS_PART_TIME:
+	    	   System.out.println("Employer is absent");
+	    	   noHours = noHours + PART_DAY_HOUR;
+	    	   noDays = noDays +1;
+	    	   break;
+	        default:
+	    	   System.out.println("Employer is absent");
+	    	
+	    }
+	    dailyempwage = noHours * WAGE_PER_HOUR ;
+	    System.out.println("Daily wage-"+ dailyempwage);
+	    monthlywage = dailyempwage * NO_OF_WORK_DAYS;
+	    System.out.println("Monthlywage-" + monthlywage);
 	}
-
 }
+}
+
 
